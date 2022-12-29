5155c835cf65b54e651b6c5b816ab4cd8fde4b2b
Srikanth Reddy <bandarisrikanth741@gmail.com>
2022-12-29 08:19:03 +0530

    added Prime Number

---
+++ Logical Programming/Logical Programming/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Logical_Programming
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Fibonacci_Series.PrintingFibonacci();
+           // PerfectNumber.FindingPerfectNumber();
+            PrimeNumber.PrintThePrimeNum();
+        }
+    }
+}
